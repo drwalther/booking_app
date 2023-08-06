6249602836200c8140738a82958e14d0d3a8dc38
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-08-06 13:06:40 +0300

    add project structure and db connection

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+from config import DB_URI
+
+engine = create_async_engine(DB_URI)
+
+session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
