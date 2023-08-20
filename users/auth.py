182cb71897fb296b4cfa577346639cbfb93485ab
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-08-20 20:32:01 +0300

    chore: add user login funtion

--- users/auth.py
+++ users/auth.py
@@ -1,4 +1,8 @@
+from fastapi import HTTPException
 from passlib.context import CryptContext
+from pydantic import EmailStr
+
+from users.service import UsersService
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +13,12 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+async def login_user(email: EmailStr, password: str):
+    user = await UsersService.get_one(email=email)
+    if not user and not verify_password(password, user.password):
+        raise HTTPException(
+            status_code=409, detail="User is not exists or password is incorrect"
+        )
+    return user
