5a09095f0a2737e8f5242f958afe1e19af47c76b
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-08-20 22:03:58 +0300

    feat: add endpoint for user log in

--- users/router.py
+++ users/router.py
@@ -1,20 +1,36 @@
 from fastapi import (
     APIRouter,
     HTTPException,
+    Response,
+    status,
 )
 
-from users.auth import get_password_hash
-from users.schemas import SchemaUserRegister
+from users.auth import (
+    authenticate_user,
+    create_access_token,
+    get_password_hash,
+)
+from users.schemas import SchemaUserAuth
 from users.service import UsersService
 
 router = APIRouter(prefix="/auth", tags=["Auth and users"])
 
 
 @router.post("/register")
-async def register_user(user_data: SchemaUserRegister):
+async def register_user(user_data: SchemaUserAuth):
     existing_user = await UsersService.get_one(email=user_data.email)
     if existing_user:
         # it seems more correct than code 422
         raise HTTPException(status_code=409, detail="User already exists")
     hashed_password = get_password_hash(user_data.password)
     await UsersService.add(email=user_data.email, hashed_password=hashed_password)
+
+
+@router.post("/login")
+async def login_user(response: Response, user_data: SchemaUserAuth):
+    user = await authenticate_user(email=user_data.email, password=user_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    access_token = create_access_token({"sub": user.id})
+    response.set_cookie("booking_access_token", access_token)
+    return access_token
