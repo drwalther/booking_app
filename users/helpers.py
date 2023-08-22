55247404a308a87d753e9869e4aaee1465145ded
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-08-22 21:52:17 +0300

    chore: add function for current user token decoding

--- users/helpers.py
+++ users/helpers.py
@@ -1,11 +1,52 @@
+from datetime import datetime
+
 from fastapi import (
+    Depends,
     HTTPException,
     Request,
+    status,
 )
+from jose import (
+    JWTError,
+    jwt,
+)
+
+from config import (
+    ALGORITHM,
+    SECRET_KEY,
+)
+from users.service import UsersService
 
 
 def get_token(request: Request):
     token = request.cookies.get("booking_access_token")
     if not token:
-        raise HTTPException(status_code=401)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Token not found"
+        )
     return token
+
+
+async def get_current_user(token: str = Depends(get_token)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Decoding fail"
+        )
+    expire: str = payload.get("exp")
+    if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Expire is incorrect"
+        )
+    user_id: str = payload.get("sub")
+    if not user_id:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="User ID is incorrect"
+        )
+    user = await UsersService.get_by_id(int(user_id))
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="User is not found"
+        )
+    return user
