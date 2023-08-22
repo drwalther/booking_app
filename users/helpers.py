c8ce8716881de01fa2d7797b83611462843f650b
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-08-22 19:32:08 +0300

    chore: add function for receiving user's access token

---
+++ users/helpers.py
@@ -0,0 +1,11 @@
+from fastapi import (
+    HTTPException,
+    Request,
+)
+
+
+def get_token(request: Request):
+    token = request.cookies.get("booking_access_token")
+    if not token:
+        raise HTTPException(status_code=401)
+    return token
