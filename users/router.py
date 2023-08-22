acec5f3f4bb04c81eb07b89f525ce9b1289a7322
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-08-22 21:57:38 +0300

    chore: add function to get bookings for current user

--- users/router.py
+++ users/router.py
@@ -33,6 +33,6 @@ async def login_user(response: Response, user_data: SchemaUserAuth):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="User doesn't exists"
         )
-    access_token = create_access_token({"subject": str(user.id)})
+    access_token = create_access_token({"sub": str(user.id)})
     response.set_cookie("booking_access_token", access_token, httponly=True)
     return access_token
