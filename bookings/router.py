dfeb0098753e5d836bd9f2f9d91a5cb0b70d852d
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-08-22 21:57:38 +0300

    chore: add function to get bookings for current user

--- bookings/router.py
+++ bookings/router.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter
+from fastapi import (
+    APIRouter,
+    Depends,
+)
 
 from bookings.schemas import SchemaBooking
 from bookings.service import BookingsService
+from users.helpers import get_current_user
+from users.models import Users
 
 router = APIRouter(prefix="/bookings", tags=["Bookings"])
 
 
 @router.get("")
-async def get_bookings() -> list[SchemaBooking]:
-    return await BookingsService.get_all()
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[SchemaBooking]:
+    return await BookingsService.get_all(user_id=user.id)
