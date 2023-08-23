c6b189e52671cabcbf750c05f7439e0db5030d50
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-08-23 23:04:12 +0300

    refactor: add custom exceptions

--- users/helpers.py
+++ users/helpers.py
@@ -2,9 +2,7 @@ from datetime import datetime
 
 from fastapi import (
     Depends,
-    HTTPException,
     Request,
-    status,
 )
 from jose import (
     JWTError,
@@ -15,15 +13,20 @@ from config import (
     ALGORITHM,
     SECRET_KEY,
 )
+from exception import (
+    InvalidTokenException,
+    TokenExpiredException,
+    TokenIncorrectException,
+    TokenNotFoundException,
+    UserNotFoundException,
+)
 from users.service import UsersService
 
 
 def get_token(request: Request):
     token = request.cookies.get("booking_access_token")
     if not token:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Token not found"
-        )
+        raise TokenNotFoundException()
     return token
 
 
@@ -31,22 +34,14 @@ async def get_current_user(token: str = Depends(get_token)):
     try:
         payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
     except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Decoding fail"
-        )
+        raise TokenIncorrectException()
     expire: str = payload.get("exp")
     if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Expire is incorrect"
-        )
+        raise TokenExpiredException()
     user_id: str = payload.get("sub")
     if not user_id:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="User ID is incorrect"
-        )
+        raise InvalidTokenException()
     user = await UsersService.get_by_id(int(user_id))
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="User is not found"
-        )
+        raise UserNotFoundException()
     return user
--- users/router.py
+++ users/router.py
@@ -1,11 +1,13 @@
 from fastapi import (
     APIRouter,
     Depends,
-    HTTPException,
     Response,
-    status,
 )
 
+from exception import (
+    UserAlreadyExistsException,
+    UserDoesNotExistException,
+)
 from users.auth import (
     authenticate_user,
     create_access_token,
@@ -23,8 +25,7 @@ router = APIRouter(prefix="/auth", tags=["Auth and users"])
 async def register_user(user_data: SchemaUserAuth):
     existing_user = await UsersService.get_one(email=user_data.email)
     if existing_user:
-        # it seems more correct than code 422
-        raise HTTPException(status_code=409, detail="User already exists")
+        raise UserAlreadyExistsException()
     hashed_password = get_password_hash(user_data.password)
     await UsersService.add(email=user_data.email, hashed_password=hashed_password)
 
@@ -33,9 +34,7 @@ async def register_user(user_data: SchemaUserAuth):
 async def login_user(response: Response, user_data: SchemaUserAuth):
     user = await authenticate_user(email=user_data.email, password=user_data.password)
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="User doesn't exists"
-        )
+        raise UserDoesNotExistException()
     access_token = create_access_token({"sub": str(user.id)})
     response.set_cookie("booking_access_token", access_token, httponly=True)
     return {"access_token": access_token}
