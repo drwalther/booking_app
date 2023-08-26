96db46d88cdb585af38cfd83b8cdc87bfb2f6e24
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-08-26 18:48:00 +0300

    feat: add function to add new booking

--- bookings/router.py
+++ bookings/router.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from fastapi import (
     APIRouter,
     Depends,
@@ -5,6 +7,7 @@ from fastapi import (
 
 from bookings.schemas import SchemaBooking
 from bookings.service import BookingsService
+from exception import RoomIsNotAvailable
 from users.helpers import get_current_user
 from users.models import Users
 
@@ -14,3 +17,15 @@ router = APIRouter(prefix="/bookings", tags=["Bookings"])
 @router.get("")
 async def get_bookings(user: Users = Depends(get_current_user)) -> list[SchemaBooking]:
     return await BookingsService.get_all(user_id=user.id)
+
+
+@router.post("")
+async def add_booking(
+    room_id: int,
+    check_in_date: date,
+    check_out_date: date,
+    user: Users = Depends(get_current_user),
+):
+    booking = await BookingsService.add(user.id, room_id, check_in_date, check_out_date)
+    if not booking:
+        raise RoomIsNotAvailable
