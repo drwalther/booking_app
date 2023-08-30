9bb7958688ef2ca417681f73d390b985bd5ea406
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-08-30 21:56:06 +0300

    fix: fix path in get_hotel_by_id

--- hotels/router.py
+++ hotels/router.py
@@ -6,6 +6,6 @@ from hotels.service import HotelsService
 router = APIRouter(prefix="/hotels", tags=["Hotels"])
 
 
-@router.get("/hotels/{hotel_id}")
+@router.get("/id/{hotel_id}")
 async def get_hotel_by_id(hotel_id: int) -> SchemaHotels:
     return await HotelsService.get_by_id(hotel_id)
