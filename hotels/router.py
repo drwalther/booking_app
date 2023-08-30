92edc4f4199c1cd47453cac77e3fe4c154be56b0
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-08-30 21:53:00 +0300

    feat: add endpoint to get hotel by id

--- hotels/router.py
+++ hotels/router.py
@@ -1,3 +1,11 @@
 from fastapi import APIRouter
 
+from hotels.schemas import SchemaHotels
+from hotels.service import HotelsService
+
 router = APIRouter(prefix="/hotels", tags=["Hotels"])
+
+
+@router.get("/hotels/{hotel_id}")
+async def get_hotel_by_id(hotel_id: int) -> SchemaHotels:
+    return await HotelsService.get_by_id(hotel_id)
