bbb8755de847321c8deeba935d6999348d0ad296
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-09-06 22:21:46 +0300

    feat: add endpoint to get hotels in specific location and date

--- hotels/router.py
+++ hotels/router.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from fastapi import APIRouter
 
 from hotels.schemas import SchemaHotels
@@ -9,3 +11,10 @@ router = APIRouter(prefix="/hotels", tags=["Hotels"])
 @router.get("/id/{hotel_id}")
 async def get_hotel_by_id(hotel_id: int) -> SchemaHotels:
     return await HotelsService.get_by_id(hotel_id)
+
+
+@router.get("/{location}")
+async def get_hotel_by_location(
+    location: str, check_in_date: date, check_out_date: date
+) -> list[SchemaHotels]:
+    return await HotelsService.get_all(location, check_in_date, check_out_date)
