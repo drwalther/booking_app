27213a6bdc23c41282f4d67a5bdd54776c37a935
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-09-11 21:01:38 +0300

    test: add app mode

--- app/config.py
+++ app/config.py
@@ -5,6 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_URI = os.getenv("DB_URI")
+TEST_DB_URI = os.getenv("TEST_DB_URI")
+
+# Development, testing or production mode
+MODE = os.getenv("MODE")
+
 # JWT token
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
