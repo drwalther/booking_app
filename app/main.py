b801e2b9cbfc869adc520838cf9b8ea4933b5b62
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-09-11 12:48:46 +0300

    chore: add CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.bookings.router import router as router_bookings
 from app.hotels.router import router as router_hotels
@@ -11,3 +12,21 @@ app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_rooms)
 app.include_router(router_hotels)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=[
+        "Content-Type",
+        "Set-Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Authorization",
+    ],
+)
