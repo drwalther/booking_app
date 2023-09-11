1e084aa8d09b9fc7685a1eb226a4324ca22bdd55
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-09-11 11:49:27 +0300

    feat: add the endpoint to get rooms in specific hotel

--- app/rooms/router.py
+++ app/rooms/router.py
@@ -1,4 +1,18 @@
+from datetime import date
+from typing import List
+
 from fastapi import APIRouter
+
+from app.rooms.schemas import SchemaRoomInfo
+from app.rooms.service import RoomsService
 
 # room is the subset of hotel and I use hotels prefix
 router = APIRouter(prefix="/hotels", tags=["Rooms"])
+
+
+@router.get("/{hotel_id}/rooms")
+async def get_rooms(
+    hotel_id: int, date_from: date, date_to: date
+) -> List[SchemaRoomInfo]:
+    rooms = await RoomsService.get_all(hotel_id, date_from, date_to)
+    return rooms
