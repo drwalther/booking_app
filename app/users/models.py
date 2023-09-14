e37798174f7349dada9cc18f52d221ed8afcda00
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-09-14 23:03:42 +0300

    chore: add __str__ for model

--- app/users/models.py
+++ app/users/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import (
     Integer,
     String,
 )
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -13,3 +14,8 @@ class Users(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
+
+    booking = relationship("Bookings", back_populates="user")
+
+    def __str__(self):
+        return f"{self.email}"
