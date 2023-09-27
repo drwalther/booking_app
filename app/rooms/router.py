fd6309bbed70c1075ece484e7f2705fee0c6a486
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-09-27 23:02:07 +0300

    chore: add caching

--- app/rooms/router.py
+++ app/rooms/router.py
@@ -2,6 +2,8 @@ from datetime import date
 from typing import List
 
 from fastapi import APIRouter
+from fastapi_cache import JsonCoder
+from fastapi_cache.decorator import cache
 
 from app.rooms.schemas import SchemaRoomInfo
 from app.rooms.service import RoomsService
@@ -11,6 +13,7 @@ router = APIRouter(prefix="/hotels", tags=["Rooms"])
 
 
 @router.get("/{hotel_id}/rooms")
+@cache(expire=60, coder=JsonCoder)
 async def get_rooms(
     hotel_id: int, date_from: date, date_to: date
 ) -> List[SchemaRoomInfo]:
