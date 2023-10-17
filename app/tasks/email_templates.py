fc78bed647423ad736b5f62f5c33550ef60def2f
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-10-17 22:42:00 +0300

    chore: add booking confirmation template

---
+++ app/tasks/email_templates.py
@@ -0,0 +1,23 @@
+from email.message import EmailMessage
+
+from pydantic import EmailStr
+
+from app.config import settings
+
+
+def create_booking_confirmation(booking: dict, email_to: EmailStr):
+    email = EmailMessage()
+
+    email["Subject"] = "Подтверждение бронирования"
+    email["From"] = settings.EMAIL_USER
+    email["To"] = email_to
+
+    email.set_content(
+        f"""
+            <h1>Подтверждение бронирования</h1>
+            Номер успешно забронирован!
+            Период: {booking["check_in_date"]} - {booking["check_out_date"]}
+        """,
+        subtype="html",
+    )
+    return email
