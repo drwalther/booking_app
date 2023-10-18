03c0159885fb3f7e84c55d962778c6bd7628bae9
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-10-18 17:57:10 +0300

    chore: add task for registration confirmation

--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -1,11 +1,20 @@
 from pydantic import EmailStr
 
 from app.tasks.celery import celery
-from app.tasks.email_templates import create_booking_confirmation
+from app.tasks.email_templates import (
+    create_booking_confirmation,
+    create_registration_confirmation,
+)
 from app.tasks.helpers import send_message
 
 
 @celery.task
 def send_booking_confirmation_email(booking: dict, email: EmailStr):
     message = create_booking_confirmation(booking, email)
+    send_message(message)
+
+
+@celery.task
+def send_registration_confirmation_email(email: EmailStr):
+    message = create_registration_confirmation(email)
     send_message(message)
