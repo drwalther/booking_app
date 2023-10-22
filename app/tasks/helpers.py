d3eacf757b4521e788b4c6aa6dad3cabaa684d38
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-10-22 14:17:59 +0300

    chore: add message creation function

--- app/tasks/helpers.py
+++ app/tasks/helpers.py
@@ -1,6 +1,8 @@
 import smtplib
 from email.message import EmailMessage
 
+from pydantic import EmailStr
+
 from app.config import settings
 
 
@@ -9,3 +11,12 @@ def send_message(message: EmailMessage):
     with smtplib.SMTP_SSL(settings.SMTP_URI, settings.SMTP_PORT) as server:
         server.login(settings.EMAIL_USER, settings.EMAIL_PASS)
         server.send_message(message)
+
+
+def create_message(email_to: EmailStr, subject: str, content: str):
+    email = EmailMessage()
+    email["Subject"] = subject
+    email["From"] = settings.EMAIL_USER
+    email["To"] = email_to
+    email.set_content(content, subtype="html")
+    return email
