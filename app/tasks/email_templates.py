1dc0d8fd048a316d17ce446be3ce2f678509c896
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-10-22 14:21:40 +0300

    chore: add type hint

--- app/tasks/email_templates.py
+++ app/tasks/email_templates.py
@@ -2,39 +2,27 @@ from email.message import EmailMessage
 
 from pydantic import EmailStr
 
-from app.config import settings
+from app.tasks.helpers import create_message
 
 
-def create_booking_confirmation(booking: dict, email_to: EmailStr):
-    email = EmailMessage()
+def create_booking_confirmation(booking: dict, email_to: EmailStr) -> EmailMessage:
+    subject = "Подтверждение бронирования"
+    content = f"""
+#             <h1>Подтверждение бронирования</h1>
+#             Номер успешно забронирован!
+#             Период: {booking["check_in_date"]} - {booking["check_out_date"]}
+#         """
 
-    email["Subject"] = "Подтверждение бронирования"
-    email["From"] = settings.EMAIL_USER
-    email["To"] = email_to
-
-    email.set_content(
-        f"""
-            <h1>Подтверждение бронирования</h1>
-            Номер успешно забронирован!
-            Период: {booking["check_in_date"]} - {booking["check_out_date"]}
-        """,
-        subtype="html",
-    )
-    return email
+    message = create_message(email_to, subject, content)
+    return message
 
 
-def create_registration_confirmation(email_to: EmailStr):
-    email = EmailMessage()
-
-    email["Subject"] = "Подтверждение регистрации"
-    email["From"] = settings.EMAIL_USER
-    email["To"] = email_to
-
-    email.set_content(
-        """
+def create_registration_confirmation(email_to: EmailStr) -> EmailMessage:
+    subject = "Подтверждение регистрации"
+    content = """
             <h1>Регистрация прошла успешно</h1>
             Теперь вы можете пользоваться сервисом.
-        """,
-        subtype="html",
-    )
-    return email
+        """
+
+    message = create_message(email_to, subject, content)
+    return message
